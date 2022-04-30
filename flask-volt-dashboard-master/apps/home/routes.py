855f8377be4ee0a6baf0e92951db523e56b8127a
Andreu Vall Hernàndez <andreu.vallhernandez@gmail.com>
2022-04-30 23:06:08 +0200

    mmmm???

--- flask-volt-dashboard-master/apps/home/routes.py
+++ flask-volt-dashboard-master/apps/home/routes.py
@@ -6,7 +6,7 @@ Copyright (c) 2019 - present AppSeed.us
 from itertools import groupby
 from re import U
 from apps.home import blueprint
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, current_app, flash
 from flask_login import login_required, current_user
 from jinja2 import TemplateNotFound
 from flask import session
@@ -21,14 +21,46 @@ import os
 df_sales = ""
 first_time = True
 
+import os
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @blueprint.route('/')
 def index():
     return render_template('home/index.html', segment='index')
 
 
-@blueprint.route('/dashboard')
+@blueprint.route('/dashboard', methods = ('GET', 'POST'))
 def dashboard():
+
+    if request.method == 'POST':
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+        filename = secure_filename(file.filename)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('home_blueprint.dashboard'))
+
     if not current_user.is_authenticated:
         return redirect(url_for('authentication_blueprint.login', form='login_form'))
     
@@ -69,9 +101,9 @@ def gm(grouped_by='Days'):
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
+
 @blueprint.route('/settings')
 def settings():
     if not current_user.is_authenticated:
         return redirect(url_for('authentication_blueprint.login', form = 'login_form'))
     return render_template('home/settings.html', segment='dashboard')
-
